9a2e1eccebf8a481d718e41bf8c97caea7e017ea
Fadyy22 <fadyalaa441@gmail.com>
2024-05-06 19:40:14 +0200

    rearrange imports

--- controllers/opportunityController.js
+++ controllers/opportunityController.js
@@ -1,5 +1,5 @@
-const { PrismaClient } = require('@prisma/client');
 const asyncHandler = require('express-async-handler');
+const { PrismaClient } = require('@prisma/client');
 
 const ApiError = require('../utils/apiError');
 
