473211e26a89e95c0a50fa73b8707ef179bbe10f
Fadyy22 <fadyalaa441@gmail.com>
2024-05-22 15:25:38 +0200

    add response header acao

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 mountRotues(app);
 
 app.use((error, req, res, next) => {
