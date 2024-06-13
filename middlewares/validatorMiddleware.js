0746bd873ccd8d09c97993635ed51bfd3ff2a0b9
Fadyy22 <fadyalaa441@gmail.com>
2024-06-13 23:28:56 +0200

    delete any images if there is validation error

--- middlewares/validatorMiddleware.js
+++ middlewares/validatorMiddleware.js
@@ -1,8 +1,11 @@
+const deleteImages = require('../utils/deleteImages');
 const { validationResult } = require('express-validator');
+
 
 exports.globalValidatorMiddleware = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    deleteImages(req);
     return res.status(400).json({ error: errors.array(), message: 'Validation Failed.' });
   }
   next();
@@ -10,6 +13,7 @@ exports.globalValidatorMiddleware = (req, res, next) => {
 
 exports.customValidatorMiddleware = (req, res, next) => {
   if (req.customError) {
+    deleteImages(req);
     return res.status(req.customError.statusCode).json({ message: req.customError.message });
   }
   next();
