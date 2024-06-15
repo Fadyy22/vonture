55fbee3c3b87c6b58c74ed1912dd9294123419a4
Fadyy22 <fadyalaa441@gmail.com>
2024-06-15 21:52:21 +0200

    add endpoint for getting all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,15 @@
+const asyncHandler = require("express-async-handler");
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+exports.getAllUsers = asyncHandler(async (req, res) => {
+  const users = await prisma.user.findMany({
+  });
+
+  res.status(200).json({ users });
+});
+
 exports.getMyProfile = async (req, res) => {
   res.status(200).json({ user: req.user });
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const {
+  getAllUsers,
   getMyProfile,
 } = require('../controllers/userController');
 
@@ -9,11 +10,17 @@ const reviewRouter = require('./reviewRoute');
 
 const isAuth = require('../middlewares/authMiddleware');
 
+const allowedTo = require('../middlewares/allowedToMiddleware');
+
 const router = express.Router();
 
 router.use('/:id/places', placeRouter);
 router.post('/:id/reviews', reviewRouter.rootReviewRouter.createUserReview);
 router.delete('/:id/reviews', reviewRouter.rootReviewRouter.deleteUserReview);
+
+router
+  .route('/')
+  .get(isAuth, allowedTo('ADMIN'), getAllUsers);
 
 router
   .route('/me')
