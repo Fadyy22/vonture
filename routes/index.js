e5880dea687c2af46c66444f888273a7a483e001
Fadyy22 <fadyalaa441@gmail.com>
2024-06-15 19:10:15 +0200

    add offers and reqs to index

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,19 @@
 const applicationRouter = require('./applicationRoute');
 const authRouter = require('./authRoute');
+const offerRouter = require('./offerRoute');
 const opportunityRouter = require('./opportunityRoute');
 const placeRouter = require('./placeRoute');
+const requirementRouter = require('./requirementRoute');
 const subscriptionRouter = require('./subscriptionRoute');
 const userRouter = require('./userRoute');
 
 const mountRoutes = (app) => {
   app.use('/applications', applicationRouter);
   app.use('/auth', authRouter);
+  app.use('/offers', offerRouter);
   app.use('/opportunities', opportunityRouter);
   app.use('/places', placeRouter);
+  app.use('/requirements', requirementRouter);
   app.use('/subscribe', subscriptionRouter);
   app.use('/users', userRouter);
 };
