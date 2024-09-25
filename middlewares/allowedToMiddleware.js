3996b1544e60c534e6c353987b767d28cf5fe12c
Fadyy22 <fadyalaa441@gmail.com>
2024-09-25 22:55:47 +0300

    remove async from allowedTo Middleware

--- middlewares/allowedToMiddleware.js
+++ middlewares/allowedToMiddleware.js
@@ -1,13 +1,12 @@
-const asyncHandler = require("express-async-handler");
-
 const ApiError = require('../utils/apiError');
 
-const allowedTo = (...roles) =>
-  asyncHandler(async (req, res, next) => {
+const allowedTo =
+  (...roles) =>
+  (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return next(new ApiError('Unauthorized.', 403));
     }
     next();
-  });
+  };
 
 module.exports = allowedTo;
